f7af8e3d4d7b273eb17750a06c29aabac0c28106
佐々木　謙 <nkekisasa222@gmail.com>
2021-11-20 16:27:56 +0900

    Initial Commit

---
+++ ATTrackingApp/ATTrackingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ATTrackingAppApp.swift
+//  ATTrackingApp
+//
+//  Created by sasaki.ken on 2021/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ATTrackingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
